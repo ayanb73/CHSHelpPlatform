74d8e3f0af9772562ec9fc498715b0fe0478b5c8
Ayan Bhattacharjee <abhattacharjee.me@gmail.com>
2017-09-14 21:01:00 -0400

    creating rooms ui service, bootstrap

--- index.js
+++ index.js
@@ -67,19 +67,10 @@ function Room(name) {
     };
     this.lines.set(uniqueEventID,lineObject);
     io.to(lineObject.stu).emit('new-line',lineObject); // sends out line object to student
-    io.to(lineObject.teach).emit('new-line',lineObject); // sends out lien object to teacher
+    io.to(lineObject.teach).emit('new-line',lineObject); // sends out line object to teacher
     this.updateLineIndex();
   };
 }
-
-// the room architecture
-const AllConnected = new Room("Big"); // all users belong to this upon google verification
-// rooms
-const AlgebraOne = new Room("Alg1");
-const Geometry = new Room("Geo");
-// room directory
-const identifiersArray = [['Alg1',AlgebraOne],['Geo',Geometry]];
-const roomIdentifiers = new Map(identifiersArray);
 
 // sending files (Express middleware)
 app.get('/', function(req, res){
@@ -105,59 +96,13 @@ io.on('connection', function(socket){
       */
       if (payload.name.includes("(")) { // student on domain
         socket.emit('verify',true,'student');
-        AllConnected.students.set(socket.id,payload.name);
       } else { // teacher
         socket.emit('verify',true,'teacher');
-        AllConnected.teachers.set(socket.id,payload.name);
       }
     });
   });
-  // event listener for join requests
-  socket.on('joinRequest', (room,user) => {
-    socket.join(room);
-    roomIdentifiers.get(room).join(user,socket.id);
-  });
-  // event listener for leave requests
-  socket.on('leaveRequest', (room,user) => {
-    socket.leave(room);
-    roomIdentifiers.get(room).leave(user,socket.id);
-  });
-  // update all clients on which rooms are active
-  setInterval(function(){
-    var liveRoomUpdate = [];
-    for (var [key,value] of roomIdentifiers) {
-      if (value.live()) {
-        liveRoomUpdate.push(key);
-      }
-    }
-    io.emit('live rooms update',liveRoomUpdate)
-  }, 1000);
-  // general message event listener
-  socket.on('msg', (msg,room,sender) => {
-    io.in(room).emit('msg',msg,room,sender);
-  });
-  // help-line request event listener
-  socket.on('help req', (room,student) => {
-    roomIdentifiers.get(room).helpLine(student,socket.id);
-  });
-  // line message event listener
-  socket.on('line-message', (eventID,msg,room,sender) => {
-    var whichRoom = roomIdentifiers.get(room);
-    var transmit = whichRoom.lines.get(eventID);
-    if (sender.occupation == 'student') {
-      io.to(transmit.teach).emit('line-message',eventID,msg,room,sender);
-    } else if (sender.occupation == 'teacher') {
-      io.to(transmit.stu).emit('line-message',eventID,msg,room,sender);
-    }
-  });
-  // disconnect event listener, which removes client from all records
-  socket.on('disconnect', function() {
-    for (var [key,value] of roomIdentifiers) {
-      value.teachers.delete(socket.id);
-      value.students.delete(socket.id);
-    }
-    AllConnected.teachers.delete(socket.id);
-    AllConnected.students.delete(socket.id);
+  socket.on('new room',(name) => {
+    socket.broadcast.emit('new room',name);
   });
 });
 // http sever up and running confirmation
--- src/app.js
+++ src/app.js
@@ -31,59 +31,6 @@ function onSignIn(googleUser) {
   });
 }
 
-// live rooms update at 1000ms intervals from the server
-socket.on('live rooms update', (array) => {
-  if (array.includes('Alg1')) {
-    vm.liveRooms.Alg1 = true;
-  } else {vm.liveRooms.Alg1 = false;}
-  if (array.includes('Geo')) {
-    vm.liveRooms.Geo = true;
-  } else {vm.liveRooms.Geo = false;}
-});
-// general messages, which are only sent by teachers, viewable by all
-socket.on('msg',(msg,room,sender) => {
-  switch(room) {
-    case 'Alg1':
-      $('#TeacherAlg1ChatBox').append("<div>"+"From: <span>"+sender.name+"</span> To: <span>"+room+"</span><br>"+msg+"</div>");
-      $('#StudentAlg1ChatBox').append("<div>"+"From: <span>"+sender.name+"</span> To: <span>"+room+"</span><br>"+msg+"</div>");
-      break;
-    case 'Geo':
-      $('#TeacherGeoChatBox').append("<div>"+"From: <span>"+sender.name+"</span> To: <span>"+room+"</span><br>"+msg+"</div>");
-      $('#StudentGeoChatBox').append("<div>"+"From: <span>"+sender.name+"</span> To: <span>"+room+"</span><br>"+msg+"</div>");
-      break;
-  }
-});
-// establishes a new help-line of communcation between one teacher and one student in a room
-socket.on('new-line',(newLine) => {
-  if (vm.my.occupation == "teacher") {
-    switch(newLine.room) {
-      case 'Alg1':
-        vm.teacher.Alg1.currentLines.push(newLine);
-      case 'Geo':
-        vm.teacher.Geo.currentLines.push(newLine);
-    }
-  } else if (vm.my.occupation == "student") {
-    switch(newLine.room) {
-      case 'Alg1':
-        vm.student.Alg1.currentLine = newLine;
-        vm.student.Alg1.hasLine = true;
-      case 'Geo':
-        vm.student.Geo.currentLine = newLine;
-        vm.student.Geo.hasLine = true;
-    }
-  }
-});
-// a message on an established help-line
-socket.on('line-message', (eventID,msg,room,sender) => {
-  if (sender.occupation == 'teacher') {
-    // student html, style difference
-    $('#'+eventID).append("<div>"+"From: <span>"+sender.name+"</span> To: <span>"+vm.my.name+"</span><br>"+msg+"</div>");
-  } else if (sender.occupation == 'student') {
-    // teacher html, style difference
-    $('#'+eventID).append("<div>"+"From: <span>"+sender.name+"</span> To: <span>"+vm.my.name+"</span><br>"+msg+"</div>");
-  }
-});
-
 // vue.js app, which dynamically controls most parrts of the application
 var vm = new Vue({
   el: '#app',
@@ -92,70 +39,22 @@ var vm = new Vue({
       name: "",
       occupation: ""
     },
+    rooms: [],
     signedIn: false,
-    connections: {Alg1: false, Geo: false},
-    liveRooms: {Alg1: false, Geo: false},
-    teacher: {
-      is: false,
-      Alg1: {
-        currentInput: "", // general message
-        currentReply: "", // line message
-        currentLines: [],
-        currentStudent: {}
-      },
-      Geo: {
-        currentInput: "", // general message
-        currentReply: "", // line message
-        currentLines: [],
-        currentStudent: {}
-      }
-    },
     student: {
-      is: false,
-      Alg1: {
-        currentInput: "",
-        currentLine: {},
-        hasLine: false
-      },
-      Geo: {
-        currentInput: "",
-        currentLine: {},
-        hasLine: false
-      }
+      is: false
+    },
+    teacher: {
+      is: false
     }
   },
   methods: {
-    joinRoom: function(room) {
-      socket.emit('joinRequest',room,this.my); // joins a room on the server
-      switch(room) { // adjusts boolean on client
-        case 'Alg1':
-          this.connections.Alg1 = true;
-          break;
-        case 'Geo':
-          this.connections.Geo = true;
-          break;
-      }
-    },
-    leaveRoom: function(room) {
-      socket.emit('leaveRequest',room,this.my); // leaves a room on the server
-      switch(room) { // adjusts boolean on client
-        case 'Alg1':
-          this.connections.Alg1 = false;
-          break;
-        case 'Geo':
-          this.connections.Geo = false;
-          break;
-      }
-    },
-    message: function(msg,room) {
-      socket.emit('msg',msg,room,this.my); // method for general room message
-    },
-    helpMe: function(room) {
-      socket.emit('help req',room,this.my); // help-line request, teacher only
-    },
-    lineMessage: function(eventID,msg,room) {
-      socket.emit('line-message',eventID,msg,room,this.my); // line message on help, adjusts client html
-      $('#'+eventID).append("<div>"+"From: <span>"+this.my.name+"</span><br>"+msg+"</div>");
+    newRoom: function(name) {
+      socket.emit('new room',{name: name});
+      this.rooms.push({name: name});
+      socket.on('new room',(room) => {
+        this.rooms.push(room);
+      });
     }
   }
 });
